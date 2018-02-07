5b5684fa9ae232a29fd7f7898af0f39ec4af9460
ranimol <abcd@gmail.com>
2018-02-07 15:33:41 +0530

    arithmetic operations

---
+++ arithop.sh
@@ -0,0 +1,8 @@
+echo "Enter two numbers"
+read a
+read b
+echo "sum is $((a+b))"
+echo "difference is $((a-b))"
+echo "product is $((a*b))"
+echo "Quotient is $((a/b))"
+echo "Remainder is $((a%b))"
